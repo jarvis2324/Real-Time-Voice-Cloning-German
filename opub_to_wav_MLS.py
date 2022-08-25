b5f98aba5b80e954a4029ad20963b9f5aa8aef28
jarvis2324 <gauravbarua614@gmail.com>
2022-08-25 19:57:22 +0530

    added opus_to_wav_MLS.py

---
+++ opub_to_wav_MLS.py
@@ -0,0 +1,13 @@
+import argparse
+import os
+import csv
+from glob import glob
+import codecs
+import subprocess
+import shutil
+datasets_root = '/mnt/disks/drive_1000/Download/mls_german_opus/train'
+wav_folders = []
+print("Searching speakers...")
+#speaker dir
+speaker_search_dir = os.path.join(datasets_root, "audio//*")
+speaker_dirs = glob(speaker_search_dir)
